3edcc3bf4e54aed3c6d78c3aaaa4fa6af913bcc3
Ubuntu <ubuntu@ip-10-0-12-195.us-west-2.compute.internal>
2019-09-27 22:22:41 +0000

    edit .gitignore to ignore fmllr feature folder and nohup.out; For egs/librispeech/s5: 1)change cmd.sh to use run.pl; 2)changed run.sh to download corpus; 3)added prepare_for_pytorch_kaldi.sh to extract fmmlr feature for train_clean_100, dev_clean, test_clean.

--- egs/librispeech/s5/run.sh
+++ egs/librispeech/s5/run.sh
@@ -4,13 +4,13 @@
 # Set this to somewhere where you want to put your data, or where
 # someone else has already put it.  You'll want to change this
 # if you're not on the CLSP grid.
-data=/export/a15/vpanayotov/data
+data=/data/LibriSpeech-asr-corpus-SLR12
 
 # base url for downloads.
-data_url=www.openslr.org/resources/12
+#data_url=www.openslr.org/resources/12
 lm_url=www.openslr.org/resources/11
 mfccdir=mfcc
-stage=1
+stage=2
 
 . ./cmd.sh
 . ./path.sh
@@ -24,9 +24,9 @@ if [ $stage -le 1 ]; then
   # download the data.  Note: we're using the 100 hour setup for
   # now; later in the script we'll download more and use it to train neural
   # nets.
-  for part in dev-clean test-clean dev-other test-other train-clean-100; do
-    local/download_and_untar.sh $data $data_url $part
-  done
+  #for part in dev-clean test-clean dev-other test-other train-clean-100; do
+  #  local/download_and_untar.sh $data $data_url $part
+  #done
 
 
   # download the LM resources
