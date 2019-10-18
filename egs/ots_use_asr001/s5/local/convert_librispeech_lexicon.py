c3a459f630242d67ed76abb3f8ecd44ded4bae60
Ubuntu <ubuntu@ip-10-0-12-251.us-west-2.compute.internal>
2019-10-18 21:54:08 +0000

    edited run_2.sh script for librispeech

--- egs/ots_use_asr001/s5/local/convert_librispeech_lexicon.py
+++ egs/ots_use_asr001/s5/local/convert_librispeech_lexicon.py
@@ -16,16 +16,119 @@ Vowels = ['AH0', 'AH1', 'AH2',
         'OW0','OW1','OW2']
 
 
+dict_map = {"AH0":"@0",
+        "AH1":"V1",
+        "AH2":"V2",
+        "AA0":"A0",
+        "AA1":"A1",
+        "AA2":"A2",
+        "AE0":"{0",
+        "AE1":"{0",
+        "AE2":"{2",
+        "ER0":"@`0",
+        "ER1":"3`1",
+        "ER2":"3`2",
+        "IH0":"I0",
+        "IH1":"I1",
+        "IH2":"I2",
+        "IY0":"i0",
+        "IY1":"i1",
+        "IY2":"i2",
+        "AO0":"O0",
+        "AO1":"O1",
+        "AO2":"O2",
+        "UH0":"U0",
+        "UH1":"U1",
+        "UH2":"U2",
+        "UW0":"u0",
+        "UW1":"u1",
+        "UW2":"u2",
+        "AY0":"aI0",
+        "AY1":"aI1",
+        "AY2":"aI2",
+        "EY0":"eI0",
+        "EY1":"eI1",
+        "EY2":"eI2",
+        "OY0":"OI0",
+        "OY1":"OI1",
+        "OY2":"OI2",
+        "AW0":"aU0",
+        "AW1":"aU1",
+        "AW2":"aU2",
+        "OW0":"oU0",
+        "OW1":"oU1",
+        "OW2":"oU2",
+        "P":"p",
+        "B":"b",
+        "D":"d",
+        "K":"k",
+        "G":"g",
+        "CH":"tS",
+        "JH":"dZ",
+        "F":"f",
+        "V":"v",
+        "TH":"T",
+        "DH":"D",
+        "S":"s",
+        "Z":"z",
+        "SH":"S",
+        "ZH":"Z",
+        "HH":"h",
+        "M":"m",
+        "N":"n",
+        "NG":"N",
+        "R":"r",
+        "L":"l",
+        "Y":"j",
+        "W":"w"
+        }
+
 librispeech_lexicon = '/home/ubuntu/src/kaldi/egs/ots_use_asr001/s5/local/librispeech-lexicon.txt'
 fid  = open(librispeech_lexicon)
 all_lexicon = fid.readlines()
 fid.close()
 
 for ln in all_lexicon:
-    temp = ln.split('\t')
-    temp2= ln.split(' ')
-    print(temp[0])
-    print(temp)
-    print(temp[1])
-    print('\n')        
+    temp = ln.strip().split('\t')
+    temp2= temp[1].split(' ')
+    new_lex = []
+    intervocalic = False
+    if "T" in temp2:
+        for i in range(len(temp2)):
+            if (i ==0) and (temp2[i]== "T"):
+                intervocalic = False
+            elif (i == len(temp2)-1) and (temp2[i]== "T"):
+                intervocalic = False
+            elif temp2[i] == "T":
+                if (temp2[i-1] in Vowels) and (temp2[i+1] in Vowels):
+                    intervocalic = True
+                    #print(ln.strip())
+                    break;
+            else:
+                continue
+    for i in range(len(temp2)):
+        if ("EH" in temp2[i]):
+            if (i+1)<= (len(temp2)-1):
+                if (temp2[i+1]== "R") :
+                    new_lex.append(temp2[i].replace("EH","E:"))
+                    continue
+                else:
+                    new_lex.append(temp2[i].replace("EH","E"))
+                    continue
+            else:
+                new_lex.append(temp2[i].replace("EH","E"))
+                continue
+        elif ("T" == temp2[i]):
+            if intervocalic:
+                new_lex.append("4")
+            else:
+                new_lex.append("t")
+        else:
+                new_lex.append(dict_map[temp2[i]])
+    print(ln.strip())
+    print(' '.join(new_lex))
+    print('\n')            
+  #      print(dict_map[temp2[i]])
+    
+    #print('\n')        
 
--- egs/ots_use_asr001/s5/run.sh
+++ egs/ots_use_asr001/s5/run.sh
@@ -8,7 +8,7 @@ data=/data/USE_ASR001
 #lm_url=www.openslr.org/resources/11
 
 mfccdir=mfcc
-stage=2
+stage=4
 
 . ./cmd.sh
 . ./path.sh
@@ -19,24 +19,9 @@ set -e
 
 
 if [ $stage -le 1 ]; then
-  # download the data.  Note: we're using the 100 hour setup for
-  # now; later in the script we'll download more and use it to train neural
-  # nets.
-  #for part in dev-clean test-clean dev-other test-other train-clean-100; do
-  #  local/download_and_untar.sh $data $data_url $part
-  #done
-
-
-  # download the LM resources
-  #local/download_lm.sh $lm_url data/local/lm
 fi
 
 if [ $stage -le 2 ]; then
-  # format the data as Kaldi data directories
-#  for part in dev-clean test-clean dev-other test-other train-clean-100; do
-    # use underscore-separated names in data directories.
-#    local/data_prep.sh $data/LibriSpeech/$part data/$(echo $part | sed s/-/_/g)
-#  done
 fi
 
 ## Optional text corpus normalization and LM training
